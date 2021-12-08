75d1207345b93506ebf8d2f6e015f1637e19f8a6
shailesh <shailaja24305@gmail.com>
2021-12-08 16:07:57 +0530

    router

---
+++ router-1/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route,} from 'react-router-dom'
+import NavBar from './Components/Navbar'
+import Contact from './Components/Contact'
+import Login from './Components/Login'
+import About from './Components/About'
+import Services from './Components/Services'
+import './style.css'
+
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+        <NavBar/>
+        <div className="container my-3">
+        <Routes>
+          <Route path="/about-us" element={<About/>}>  </Route>
+          <Route path="/contact" element={<Contact/>}> </Route>
+          <Route path="/services" element={<Services/>}> </Route>
+          <Route path="/login" element={<Login/>}> </Route>
+        </Routes>
+        </div>
+        </BrowserRouter>
+   </>
+  )
+}
+
+export default App
